3e6e9b690da35ef973922b504ab436087a2b45cf
Rizwan <contact@fainin.com>
2023-04-27 02:48:05 +0500

    T-3: Fixed (renamed) Project name

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'world-tree'`, () => {
+  it(`should have as title 'wonder-tree'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('world-tree');
+    expect(app.title).toEqual('wonder-tree');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('world-tree app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('wonder-tree app is running!');
   });
 });
