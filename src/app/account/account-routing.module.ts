b73245628557e4a6df29dc913b30854cf6da71a4
Rizwan <contact@fainin.com>
2023-04-28 19:56:50 +0500

    T-10: Set Up Account Guard

--- src/app/account/account-routing.module.ts
+++ src/app/account/account-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AccountDashboardComponent } from './account-dashboard/account-dashboard.component';
+import { AccountGuard } from '../@core/guards/account-guard.service';
 
 const routes: Routes = [
   {
     path: '',
     pathMatch: 'full',
+    canActivate: [AccountGuard],
     component: AccountDashboardComponent,
   },
 ];
